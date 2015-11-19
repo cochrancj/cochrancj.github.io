ec3a5df01eee34b0f729f7b2955ea62a4f7fb6ab
Carolyn Cochran <cochran.cj@gmail.com>
2015-11-19 17:52:49 -0500

    Not Sure What This Is - Class 7 work, maybe?

---
+++ my-class-work/class-07-intro-to-programming/first_javascript/script.js
@@ -0,0 +1,19 @@
+alert('this is an alert. this is annoying');
+
+document.write('writing the document, like a boss');
+
+document.write(10 * 10);
+
+document.write(100 / 7);
+
+document.write(50 - 17);
+
+document.write(50 + 17);
+
+document.write('Carolyn ' + 'Cochran');
+
+var num1 = 10 * 10;
+
+document.write(num1);
+
+document.getElementById('title').style.color='red';
