38f8081d4ad2bea6fcce8a8593150ed45422446b
Carolyn Cochran <cochran.cj@gmail.com>
2015-11-19 21:25:09 -0500

    CLASS 8

---
+++ my-class-work/class-08-javascript-2/lifetime-supply/js/project.js
@@ -0,0 +1,17 @@
+document.getElementById('click-me').onclick = calculateLifetimeDrinks;
+
+function calculateLifetimeDrinks () {
+  var currentAge = document.getElementById('age').value;
+  var plannedAge = document.getElementById('max-age').value;
+  var favDrink = document.getElementById('item').value;
+  var numPerDay = document.getElementById('num-per-day').value;
+
+  var lifetimeDrinks = (plannedAge - currentAge) * 365 * numPerDay;
+
+  document.getElementById('solution').innerHTML = lifetimeDrinks;
+  document.getElementById('drink').innerHTML = favDrink;
+
+  // console.log(lifetimeDrinks);
+}
+
+
---
+++ my-class-work/class-08-javascript-2/rgb-color-choice/js/project.js
@@ -0,0 +1,14 @@
+document.getElementById('color-button').onclick = changeColor;
+
+function changeColor () {
+  var red = document.getElementById('red').value;
+  var green = document.getElementById('green').value;
+  var blue = document.getElementById('blue').value;
+
+  var rgbString = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  document.body.style.backgroundColor = rgbString;
+
+  document.getElementById('colorful-text').innerHTML = rgbString;
+  
+  console.log(rgbString);
+}
