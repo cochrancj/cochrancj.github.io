554d5f763e23954064a693062b575c85d13c5e52
Carolyn Cochran <cochran.cj@gmail.com>
2015-11-24 21:34:15 -0500

    Class 9

---
+++ homework/week-05-hw/calculator-box/js/project.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+var total = 0;
+
+// Add 10 Event
+
+$('#a10').click(add10);
+
+function add10 () {
+  total = total + 10
+  $('#out').text(total)
+}
+// End Add 10 Event
+
+// DUPLICATE ^ FOR EACH
+
+$('#a20').click(add20);
+$('#a30').click(add30);
+$('#red').click(changeRed);
+$('#blue').click(changeBlue);
+$('#n10').click(minus10);
+$('#n20').click(minus20);
+$('#n30').click(minus30);
+
+
+
+$('#out').text()
+
+
+
+add10
+add20
+add30
+
+
+
+
+
+
+
+  })
+
