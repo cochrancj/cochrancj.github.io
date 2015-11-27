aa5fe1eec2526fff87a612773523a3495e7a36f4
Carolyn Cochran <cochran.cj@gmail.com>
2015-11-27 13:01:28 -0500

    Week 5 Homework Done
    Week 5 Bonus Work Half Done

--- homework/week-05-hw/calculator-box/js/project.js
+++ homework/week-05-hw/calculator-box/js/project.js
@@ -3,34 +3,71 @@ $(document).ready(function () {
 var total = 0;
 
 // Add 10 Event
-
 $('#a10').click(add10);
 
 function add10 () {
   total = total + 10
   $('#out').text(total)
 }
-// End Add 10 Event
-
-// DUPLICATE ^ FOR EACH
+// End Add 20 Event
 
 $('#a20').click(add20);
+
+function add20 () {
+  total = total + 20
+  $('#out').text(total)
+}
+// End Add 20 Event
+
 $('#a30').click(add30);
-$('#red').click(changeRed);
-$('#blue').click(changeBlue);
+
+function add30 () {
+  total = total + 30
+  $('#out').text(total)
+}
+// End Add 30 Event
+
 $('#n10').click(minus10);
+
+function minus10 () {
+  total = total - 10
+  $('#out').text(total)
+}
+// End Minus 10 Event
+
 $('#n20').click(minus20);
+
+function minus20 () {
+  total = total - 20
+  $('#out').text(total)
+}
+// End Minus 20 Event
+
 $('#n30').click(minus30);
 
+function minus30 () {
+  total = total - 30
+  $('#out').text(total)
+}
+// End Minus 30 Event
+
+$('#red').click(turnRed);
+
+function turnRed () {
+  $('#red').toggleClass('on');
+}
+// End Turn Red Event
+
+$('#blue').click(turnBlue);
+
+function turnBlue () {
+  $('#blue').toggleClass('on');
+}
+// End Turn Blue Event
+
+})
 
 
-$('#out').text()
-
-
-
-add10
-add20
-add30
 
 
 
@@ -38,5 +75,9 @@ add30
 
 
 
-  })
+
+
+
+
+ 
 
