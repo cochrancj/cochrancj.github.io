56c67c3aff1ba1160eeec51d1bce051772af8840
Carolyn Cochran <cochran.cj@gmail.com>
2015-12-10 21:35:58 -0500

    Class 13 Classwork

---
+++ my-class-work/class-13-jquery-5/form-test/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  
+  $('#myForm').submit(function (event) {
+    event.preventDefault()
+    
+    var name = $('#name-input').val()
+
+    $('#target').text(name)
+  })
+
+})
