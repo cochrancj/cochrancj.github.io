603766edce1c525bcb37cd1e6d6282bddc9bafc6
Carolyn Cochran <cochran.cj@gmail.com>
2015-12-12 11:43:24 -0500

    Working on Week 7 Homework + Final Project
    Stuck on homework
    Stuck on project
    Need to schedule office hours time

---
+++ Final_Project/js/project.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+// vvvv ROTATE BANNER (BROKEN)vvvv --- WHY??
+// should I make each banner element an image instead of a class?
+
+// I only want the text to change when the banner goes
+// to the next slide. Should I make only the text rotate, 
+// or make the whole thing rotate? 
+
+
+// $(document).ready(function () {
+//   $("#slider > div:gt(0)").hide();
+//   $("#slider").height($("#slider > div > blurb").height());
+//   setInterval(function () {
+//     $('#slider > div: first')
+//     .fadeOut(1000)
+//     .next()
+//     .fadeIn(1000)
+//     .end()
+//     .appendTo('#slider');
+//   }, 3000);
+//   })
+// })
+
+// THIS IS AN EXAMPLE OF A ROTATING BANNER: 
+
+// https://jsfiddle.net/y5tgumdo
+
+
+
+
+// -------------
+
+// vvvv ARRAY (BROKEN) vvvv  ---- WHY??
+
+// $(document).ready(function () {
+//   $('rotate').submit(function () {
+//     var recipe = ["ingredient-image", "ingredient-expanded"];
+//     $("#recipe").innerHtml = demo;
+//   })
+// })
+
+
+
+// THIS IS AN EXAMPLE OF AN ARRAY:
+// <p id="demo"></p>
+
+// <script>
+// var cars = ["Saab", "Volvo", "BMW"];
+// document.getElementById("demo").innerHTML = cars;
+// </script>
--- homework/week-07-hw/cash-register/js/index.js
+++ homework/week-07-hw/cash-register/js/index.js
@@ -4,17 +4,18 @@
 // 3: #total is updated to reflect the sum of all the #entries
 //
 // Instructions:
-// 1: Add a .submit event to the form; use an anonymous function to handle the sumbit
-// 2: Remeber to prevent the default action on the form!
-// 3: Create a variable to store the user input from #newEntry
-// 4: Remember to call parseFloat() on this variable! It needs to be a number.
-// 5: Append a new <tr></tr> to the <tbody id="entries"> 
+// ~~1: Add a .submit event to the form; use an anonymous function to handle the sumbit
+// ~~2: Remeber to prevent the default action on the form!
+// ~~3: Create a variable to store the user input from #newEntry
+// ~~4: Remember to call parseFloat() on this variable! It needs to be a number.
+// ~~5: Append a new <tr></tr> to the <tbody id="entries"> 
 // containing two <td>'s, one of which contains the new variable: 
 // <tr><td></td><td>NEW VALUE</td></tr>
+
+
 // 6: Figure out a way to update the number in #total (Hint: look back at the calculator box example)
 // 7: Empty the input in #newEntry (clear it out)
 
-// parseFloats, concatinations, prevent default action
 
 $(document).ready(function () {
 
@@ -25,10 +26,20 @@ $('#entry').submit(function (event) {
   
   var total = parseFloat(total)
 
+  // total = total + (#newEntry)
+
+  // var total = 0;
+
 $('#entries').append('<tr>'+'<td>'+'</td>'+'<td>'+total+'</td>'+'</tr>')
 
-  // $('#total').
+$("#total").html(total)
 
+
+
+//   $("#a10").click(function(){
+//     total = total + 10;
+//     $("#out").html(total);
+//   });
 
 
 })
