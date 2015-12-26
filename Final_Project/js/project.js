906357c32752d6d871adeaefdf875a77fa3badbd
Carolyn Cochran <cochran.cj@gmail.com>
2015-12-26 09:41:08 -0500

    Working On Final Project

--- Final_Project/js/project.js
+++ Final_Project/js/project.js
@@ -2,21 +2,25 @@
 
 $(document).ready(function () {
 
-  $("#slider > div:gt(0)").hide();
-  
-  $("#slider").height($("#slider > div > blurb").height());
-  // setInterval(function () {
-    $("#slider > div: first")
-      .fadeOut(1000)
-      .next()
-      .fadeIn(1000)
-      .end()
-      .appendTo("#slider");
-  // }, 3000);
+  $('proteinDie').click(function () {
+    $('#recipe').append = Math.floor(Math.random()*6)+1
   })
 
-things = $('.rotate img');
-$(things[Math.floor(Math.random()*things.length)]).click()
+  // $("#slider > div:gt(0)").hide();
+  
+  // $("#slider").height($("#slider > div > blurb").height());
+  // // setInterval(function () {
+  //   $("#slider > div: first")
+  //     .fadeOut(1000)
+  //     .next()
+  //     .fadeIn(1000)
+  //     .end()
+  //     .appendTo("#slider");
+  // // }, 3000);
+  // })
+
+// things = $('.dice');
+// $(things[Math.floor(Math.random()*things.length)]).click()
 
 // this should return a random element 
 // by clicking on something in the div 
@@ -25,10 +29,12 @@ $(things[Math.floor(Math.random()*things.length)]).click()
 //   random link per click).
 
 // rename my dice as elements in a div.
-  $('.proteinDie').click(function () {
-    var recipe = ['.ingredient-image', '.protein-expanded', '.bulk-veg-expanded', '.accent-veg-expanded', '.cook-style-expanded', '.flavor-profile-expanded'];
-    $("#recipe").innerHTML = recipe;
-  })
 
-// })
+
+  // $('.proteinDie').click(function () {
+  //   var recipe = ['.ingredient-image', '.protein-expanded', '.bulk-veg-expanded', '.accent-veg-expanded', '.cook-style-expanded', '.flavor-profile-expanded'];
+  //   $("#recipe").text = recipe;
+  // })
+
+ })
 
